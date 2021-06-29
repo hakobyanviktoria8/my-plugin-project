0a9acb4e56eb83660af74d01fe0852f342e9807c
Viktorya Hakobyan <victoria@webmaker.am>
2021-06-29 09:41:00 +0400

    AWS s3 npm use

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,21 @@
 module.exports = ({ env }) => ({
-  // ...
-  email: {
+
+  //aws-s3 provider
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: 'aws-region',
+      params: {
+        Bucket: 'my-bucket',
+      },
+    },
+  },
+
+
+  // send email in mailtrap
+  /*email: {
     provider: 'mailtrap',
     providerOptions: {
       user: env('MAILTRAP_USER', 'c06ede2f6c967a'),
@@ -10,6 +25,6 @@ module.exports = ({ env }) => ({
       defaultFrom: env('MAILTRAP_DEFAULT_FROM', 'default@value.com'),
       defaultReplyTo: env('MAILTRAP_DEFAULT_REPLY_TO', 'default@value.com'),
     },
-  }
+  }*/
   // ...
 });
